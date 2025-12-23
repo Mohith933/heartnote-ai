549308c2475598d588651ff28a1314ed56e9d095
Mohith sai b <bmstpt1@gmail.com>
2025-12-23 18:17:56 +0530

    Update settings.py

--- hearnoteai/settings.py
+++ hearnoteai/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 ROOT_URLCONF = 'hearnoteai.urls'
 
 TEMPLATES = [
